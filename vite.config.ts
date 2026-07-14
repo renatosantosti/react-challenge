f8bd4779e95ba502a7fe092fecefe163b90ba9c3
Renato Santos <renatosantos.ti@gmail.com>
2026-07-14 18:36:46 -0300

    fix(settings): update Vite configuration to include React plugin and adjust API proxy path and added .ignore files.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "client",
+  plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api/": "http://localhost:3000",
     },
   },
 });
